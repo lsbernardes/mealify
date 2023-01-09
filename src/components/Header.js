3c71fe5a92a0b25ef78afa31cc9a79b30f8179fa
Leonardo Bernardes <lsbernardes@gmail.com>
2023-01-09 11:28:12 +0100

    Adjusting logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import classes from '../static/css/Header.module.css';
 const Header = () => {
   return (
     <div className={classes.nav}>
-      <img alt="My Meal" width="38px" height="38px" className={classes.nav} />
-      <h1>
+      <a href="index.html" className={classes.href}><img alt="My Meal" className={classes.nav} /></a>
+      {/* <h1>
         <a href="index.html" className={classes.href}>
           Minha Comida
         </a>
-      </h1>
+      </h1> */}
     </div>
   );
 };
